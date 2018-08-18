287eb0bde6453627c791ffed56cec39c4de46275
ittyler1221 <40411316+ittyler1221@users.noreply.github.com>
2018-08-18 14:53:18 -0700

    Add files via upload
    car insurance

---
+++ Boolean Logic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Boolean_Logic
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What Is Your Age?");
+            string Age = Console.ReadLine();
+            int Age1 = Convert.ToInt16(Age);
+            Console.WriteLine("Have You Ever Had A DUI? Answer true Or false");
+            string DUI = Console.ReadLine();
+            Boolean DUI1 = Convert.ToBoolean(DUI);
+            Console.WriteLine("How Many Speeding Tickets Do You Have?");
+            string Tickets = Console.ReadLine();
+            int Tickets1 = Convert.ToInt16(Tickets);
+            Console.WriteLine("Qualified");
+            Boolean check = (Age1 > 15 && DUI1 == false && Tickets1 < 4);
+            Console.WriteLine(check);
+            Console.ReadLine();
+
+        }
+    }
+}
