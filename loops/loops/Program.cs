cc16e5b51b9acc15d3c57d3c6d7426eb09bea654
ittyler1221 <40411316+ittyler1221@users.noreply.github.com>
2018-08-23 10:46:04 -0700

    Add files via upload
    Boolean Loops

---
+++ loops/loops/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("How many cats do you have?");
+            int cats = Convert.ToInt16(Console.ReadLine());
+            while (cats > 1)
+            {
+                Console.WriteLine("You have to many cats");
+                Console.WriteLine("Get rid Of some. How many cats do you have now?");
+                cats = Convert.ToInt16(Console.ReadLine());
+                {
+                    if (cats == 1)
+                        Console.WriteLine("You have enough cats stop it.");
+                    Console.ReadLine();
+                }
+            }
+            {
+                Console.WriteLine("How many letters are there in the Alphabet");
+                int letters = Convert.ToInt32(Console.ReadLine());
+                do
+                {
+                    if (letters == 26)
+                    {
+                        Console.WriteLine("Good Job.");
+                        Console.ReadLine();
+                        break;
+                    } else
+                    {
+                        Console.WriteLine("Keep Trying.");
+                        letters = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
+                while (letters != 1);
+            }
+        }
+    }
+}
