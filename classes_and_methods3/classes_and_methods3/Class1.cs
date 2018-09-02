8066878feacbedfbd4dbc36b9183fe96481a96a1
ittyler1221 <40411316+ittyler1221@users.noreply.github.com>
2018-09-02 16:27:41 -0700

    Add files via upload

---
+++ classes_and_methods3/classes_and_methods3/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes_and_methods3
+{
+    class Class1
+    {
+        public int Main2(int Enter1)
+        {
+            int entered = (Enter1 + 50);
+            return entered;
+        }
+
+        public decimal Main2(decimal Enter1)
+        {
+            decimal entered = Enter1 / 5;
+            return entered;
+        }
+        public int Main2(string Enter1)
+        {
+            int entered = Convert.ToInt32(Enter1);
+            entered = entered - 10;
+            return entered;
+        }
+    }
+}
---
+++ classes_and_methods3/classes_and_methods3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes_and_methods3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 addition = new Class1();
+            int enters = addition.Main2(12);
+            Console.WriteLine(enters);
+            Console.ReadLine();
+
+            Class1 division = new Class1();
+            decimal enters2 = division.Main2(.15m);
+            Console.WriteLine(enters2);
+            Console.ReadLine();
+
+            Class1 subtraction = new Class1();
+            int entered = 0;
+            int enters3 = subtraction.Main2(entered - 10);
+            Console.WriteLine(enters3);
+            Console.ReadLine();
+        }
+    }
+}
