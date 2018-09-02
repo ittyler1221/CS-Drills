458835b8c3a5909788999da769fe85d3304fd955
ittyler1221 <40411316+ittyler1221@users.noreply.github.com>
2018-09-02 14:51:05 -0700

    Add files via upload

---
+++ classes_and_methods2/classes_and_methods2/Class2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes_and_methods2
+{
+    class Class2
+    {
+        public void Main2(int Enter1, int Enter2)
+        {
+            int entered = Enter1 - 5;
+            Console.WriteLine("Number Minus 5 = ");
+            Console.WriteLine(entered);
+            Console.ReadLine();
+            Console.WriteLine(Enter2 + " is the second number entered.");
+
+
+
+        }
+    }
+}
---
+++ classes_and_methods2/classes_and_methods2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes_and_methods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class2 subtraction = new Class2();
+
+            subtraction.Main2(200, 50);
+            Console.ReadLine();
+
+            subtraction.Main2(Enter1: 35, Enter2: 70);
+            Console.ReadLine();
+
+        }
+    }
+}
