33981fdcf141917b17a656d670fab883d0eab24f
ittyler1221 <40411316+ittyler1221@users.noreply.github.com>
2018-09-02 13:14:07 -0700

    Add files via upload

---
+++ methods/methods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int Enter = Convert.ToInt16(Console.ReadLine());
+
+            methods.class1.Hello1(Enter);
+
+            methods.class1.Hello2(Enter);
+
+            methods.class1.Hello3(Enter);
+        }
+    }
+}
---
+++ methods/methods/class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    class class1
+    {
+       public static void Hello1 (int Enter)
+        {
+            Console.WriteLine("Your Number Entered Multiplied By 4 = " + (Enter * 4));
+        }
+        public static void Hello2 (int Enter)
+        {
+            Console.WriteLine("Your Number Subtracted By 4 = " + (Enter - 4));
+        }
+        public static void Hello3 (int Enter)
+        {
+            Console.WriteLine("Your Number Divided By 4 = " + (Enter / 4));
+            Console.ReadLine();
+        }
+    }
+}
