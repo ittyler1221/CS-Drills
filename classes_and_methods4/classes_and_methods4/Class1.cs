96cef3c834905d6a12a2778f5eaf3bb45585b155
ittyler1221 <40411316+ittyler1221@users.noreply.github.com>
2018-09-02 17:32:59 -0700

    Add files via upload

---
+++ classes_and_methods4/classes_and_methods4/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes_and_methods4
+{
+    class Class1
+    {
+        public int Enter (int enter1, int enter2 = 0)
+        {
+            int entered = enter1 + enter2;
+            return entered;
+        }
+    }
+}
---
+++ classes_and_methods4/classes_and_methods4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes_and_methods4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 addition = new Class1();
+
+            Console.WriteLine("Enter a number");
+            int anumber1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You can enter another number if you want");
+            string anumber2 = Console.ReadLine();
+
+            if (anumber2 == "")
+            {
+                int entered1 = addition.Enter(anumber1);
+                Console.WriteLine(anumber1 + " + 0 = " + entered1);
+                Console.ReadLine();
+            }
+            else
+            {
+                int num2 = Convert.ToInt32(anumber2);
+                int entered2 = addition.Enter(anumber1, num2);
+                Console.WriteLine(anumber1 + " + " + anumber2 + " = " + entered2);
+                Console.ReadLine();
+            }
+        }
+    }
+}
