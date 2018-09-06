acdb9295f6c61f502c498dcc2d6917475ff810a7
ittyler1221 <40411316+ittyler1221@users.noreply.github.com>
2018-09-06 15:33:25 -0700

    Add files via upload

---
+++ Structs/Structs/Struct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    public struct Number
+    {
+        internal static decimal amount;
+
+        public decimal Amount { get; set; }
+    }
+}
