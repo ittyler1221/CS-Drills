1a7b8c9288969d7294bf68ef74f157a2616150d8
ittyler1221 <40411316+ittyler1221@users.noreply.github.com>
2018-09-06 12:03:12 -0700

    Add files via upload

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Enums
+{
+    class Program
+    {
+        public enum DaysOfWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday,
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What day is it?");
+            string Enter = Console.ReadLine();
+            try
+            {
+                DaysOfWeek day = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), Enter);
+                Console.WriteLine("Oh ya your right it is " + day);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
