1d9726e9c3c82f235298c5ba7027f15196ae2f0b
ittyler1221 <40411316+ittyler1221@users.noreply.github.com>
2018-09-12 13:41:30 -0700

    Add files via upload

---
+++ Constructor/Constructor/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Person
+    {
+        public Person()
+        {      
+            Person pers = new Person();
+        }
+        public string first;
+        public string last;
+
+        public Person(string f1):this(f1,"-")
+        {
+         
+        }
+
+        public Person(string f1, string f2) 
+        {
+            first = f1;
+            last = f2;
+        }
+
+
+        public void PrintName()
+        {
+            Console.WriteLine(first + " " + last);
+        }
+    }
+}
---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int twenty = 20;
+            object Tyler = null;
+            var name = Tyler;
+
+            Person pers = new Person("Tyler", "Helm");
+            pers.PrintName();
+            Console.ReadLine();
+        }
+    }
+}
